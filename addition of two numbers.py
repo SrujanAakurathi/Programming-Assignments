557903076202aee6078b304438568a102a628bd6
Srujan Aakurathi <srujan.neo@gmail.com>
2019-09-15 18:22:57 +0530

    Programming-Assignments

---
+++ addition of two numbers.py
@@ -0,0 +1,24 @@
+"""
+
+Input Format:
+
+The first line of the input contains two numbers separated by a space.
+
+Output Format:
+
+Print the addition in single line
+
+Example:
+
+Input:
+4 2
+
+Output:
+6
+
+"""
+
+
+
+l = map(int,input().split())
+print(sum(l))
---
+++ smallest of numbers.py
@@ -0,0 +1,22 @@
+"""
+
+Given two numbers (integers) as input, print the smaller number.
+
+Input Format:
+The first line of input contains two numbers separated by a space
+
+Output Format:
+Print the smaller number
+
+Example:
+
+Input:
+2 3
+
+Output:
+2 
+
+"""
+
+n = map(int, input().split())
+print(min(n))
